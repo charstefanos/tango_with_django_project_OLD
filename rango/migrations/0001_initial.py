a87752f57baf81dd589a0e116daf01919db86ecd
Stefanos Charalambous <charstefanos@gmail.com>
2019-01-31 17:20:17 +0000

    Chapter 5 + Exercises, error likes = models.IntegerField(default=0)

--- rango/migrations/0001_initial.py
+++ rango/migrations/0001_initial.py
@@ -20,6 +20,9 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=128, unique=True)),
             ],
+			options={
+                'verbose_name_plural': 'Categories',
+			},
         ),
         migrations.CreateModel(
             name='Page',
